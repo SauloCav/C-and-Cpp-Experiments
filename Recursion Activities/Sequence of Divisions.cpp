b3024c81d29df2e9b91d3de1ace2e8c7ed773405
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-28 13:49:14 -0300

    adds Sequence of Divisions activitie

---
+++ Recursion Activities/Sequence of Divisions.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+
+float sod(float n, float cc, float i);
+
+int main(){
+	
+	float h, n, cc=0, i=1;
+	
+	printf("insira n: ");
+	scanf("%f", &n);
+	
+	h=sod(n, cc, i);
+	
+	printf("resultado: %f", h);
+	
+}
+
+float sod(float n, float cc, float i){
+	
+	if(i<=n){
+		
+		return sod(n, cc+=1/i, i+1);
+		
+	}
+	else{
+		
+		return cc;
+		
+	}
+	
+}
