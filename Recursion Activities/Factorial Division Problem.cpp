9fc40b7ef90f41fde009390187028fcba0a042ef
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-29 09:54:16 -0300

    adds factorial division problem activitie

---
+++ Recursion Activities/Factorial Division Problem.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+#include<locale.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+#include<string.h>
+
+int fat(int n, int i);
+
+int main(){
+	
+	int n, r, i=1;
+	
+	printf("informe n: ");
+	scanf("%d", &n);
+	
+	r=fat(n-1, i);
+	
+	if(n>1){
+		printf("\nS = %d", r+1);
+	}
+	else{
+		printf("\nS = %d", r);
+	}
+	
+}
+
+int fat(int n, int i){
+	
+	printf("%d ", n);
+	printf("%d\n", i);
+	
+	if(i<n){
+		return i+i*fat(n, i+1);
+	}
+	else{
+		return i;
+	}
+	
+}
