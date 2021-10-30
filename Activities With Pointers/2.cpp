701ebe7ce437c031d95cdd06283787e34179a940
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-30 10:04:21 -0300

    adds the second pointer activity

---
+++ Activities With Pointers/2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int *a, *b, c=4, d=2;
+	a=&c;
+	b=&d;
+	*b=8;
+	*a=*b;
+	*a=1;
+	b=a;
+	*b=0;
+	
+	printf("a=%d\n", a);
+	printf("b=%d\n", b);
+	printf("c=%d\n", c);
+	printf("d=%d\n\n", d);
+	
+	printf("a=%d\n", &a);
+	printf("b=%d\n", &b);
+	printf("c=%d\n", &c);
+	printf("d=%d\n", &d);
+	
+}
